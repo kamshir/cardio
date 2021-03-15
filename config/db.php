165f0e5316a07c690b7f79cbc786e126b0241642
Максим <46054418+kamshir@users.noreply.github.com>
2021-03-15 23:23:39 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cardio',
+    'dsn' => 'mysql:host=cardio;dbname=cardio',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
